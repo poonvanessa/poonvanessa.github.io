cfef736e3af424c0be5747d5d7727897462d69c0
echelper@ust.hk <iamvanessapoon@gmail.com>
2019-07-29 22:48:41 +0900

    load common parts

--- js/layout.js
+++ js/layout.js
@@ -1,3 +1,6 @@
 $(function(){
   $("head").load("common/head.html");
+  $("#header").load("common/header.html");
+  $("#nav").load("common/navigation.html");
+  $("footer").load("common/footer.html");
 });
