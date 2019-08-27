65b4201f20fec2601eabc4731c8609417128d9dc
echelper@ust.hk <iamvanessapoon@gmail.com>
2019-08-27 20:57:36 +0900

    complete education, experience and skill sections, update nav bar, update images

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -114,12 +114,12 @@ $(document).ready(function(){
     // 5. welcome animation support
 
         $(window).load(function(){
-        	$(".header-text h2,.header-text p").removeClass("animated fadeInUp").css({'opacity':'0'});
+        	$(".header-text h2,.header-text h3,.header-text p").removeClass("animated fadeInUp").css({'opacity':'0'});
             $(".header-text a").removeClass("animated fadeInDown").css({'opacity':'0'});
         });
 
         $(window).load(function(){
-        	$(".header-text h2,.header-text p").addClass("animated fadeInUp").css({'opacity':'0'});
+        	$(".header-text h2,.header-text h3,.header-text p").addClass("animated fadeInUp").css({'opacity':'0'});
             $(".header-text a").addClass("animated fadeInDown").css({'opacity':'0'});
         });
 
